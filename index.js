1d0d9d0cdd02325ff3c91dbd0a9aa3b046dc9bde
Ashley King <Princessak8918@hotmail.com>
2023-02-24 16:06:16 -0700

    pass test

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+    callback()
+}
+   function returnsANamedFunction(){
+    return function hook(){}
+   }
+function  returnsAnAnonymousFunction(){
+    return () => x
+}
+
+
+    
